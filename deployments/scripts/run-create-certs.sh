5ad9eedc8642bfd76907c1262937d1283435207e
Rogerio Bastos <2397391+rogeriobastos@users.noreply.github.com>
2021-07-02 10:47:10 -0300

    Refactore script to create TLS certificates

--- deployments/scripts/run-create-certs.sh
+++ deployments/scripts/run-create-certs.sh
@@ -6,15 +6,23 @@
 #
 # This script will create certs to build the enviroment via Makefile.
 #
-printf 'Generating Certs            ...'
-rm -rf .env
-
-if [ ! -f api/api-tls-cert.pem ] && [ ! -f api/api-tls-key.pem ]; then
-    rm -rf deployments/certs/*
-    ./deployments/scripts/create-certs.sh -m ca -pw "huskyCICertPassphrase" -t deployments/certs -e 900
-    ./deployments/scripts/create-certs.sh -m server -h dockerapi -pw "huskyCICertPassphrase" -t deployments/certs -e 365
-    ./deployments/scripts/create-certs.sh -m client -h huskyapi -pw "huskyCICertPassphrase" -t deployments/certs -e 365
-    ./deployments/scripts/create-certs.sh -m tls -h dockerapi -pw "huskyCICertPassphrase" -t api -e 365
+if [ -d $HUSKYCI_CERT_PATH ]
+then
+    echo "Skipping certificate creation because \$HUSKYCI_CERT_PATH already exist."
+    exit 0
+else
+    mkdir -p $HUSKYCI_CERT_PATH && cd $HUSKYCI_CERT_PATH
+    # CA certificate
+    $HUSKYCI_SCRIPTS/create-certs.sh -m ca -pw $HUSKYCI_CERT_PASSPHRASE -t . -e 900
+    # Docker API server certificate
+    $HUSKYCI_SCRIPTS/create-certs.sh -m server -h $HUSKYCI_DOCKERAPI_HOST -hip $HUSKYCI_DOCKERAPI_ADDR -pw $HUSKYCI_CERT_PASSPHRASE -t . -e 365
+    mkdir -p docker
+    cp -a ca.pem docker/
+    mv server-cert.pem server-key.pem docker/
+    # Docker API client certificate
+    $HUSKYCI_SCRIPTS/create-certs.sh -m client -h $HUSKYCI_DOCKERAPI_HOST -pw $HUSKYCI_CERT_PASSPHRASE -t . -e 365
+    mkdir -p api
+    cp -a ca.pem api/
+    mv cert.pem key.pem api/
+    cd -
 fi
-
-echo " done"
